d559182542023c6a3c2082da82b71d41037254e2
shahadathhs <shahadathhossensajib732@gmail.com>
2025-06-20 12:08:23 +0600

    feat: added helper file

---
+++ src/helpers/helper.js
@@ -0,0 +1,1 @@
+export const jsonContent = { 'Content-Type': 'application/json' }
--- src/utils/response.js
+++ src/utils/response.js
@@ -1,9 +1,11 @@
+import { jsonContent } from '../helpers/helper';
+
 export function sendSuccess(res, data, statusCode = 200) {
-  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.writeHead(statusCode, jsonContent);
   res.end(JSON.stringify({ success: true, data }));
 }
 
 export function sendError(res, message, statusCode = 400) {
-  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.writeHead(statusCode, jsonContent);
   res.end(JSON.stringify({ success: false, error: message }));
 }
